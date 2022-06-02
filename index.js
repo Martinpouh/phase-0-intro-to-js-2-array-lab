44a54942b41ed6c21e87bf3f7a81c050fc307653
Martinpouh <martinchege2015@gmail.com>
2022-06-02 18:41:51 +0300

    commit tree a09d710accb4f67f72e33a95d4548bd704292732

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyPrependCat(name) {
+    cats.unshift('Bob');
+}
+
+function destructivelyAppendCat(name) {
+    cats.push('Ralph');
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop('Garfield');
+}
+
+function appendCat(name) {
+    return [...cats, 'Broom'];
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift('Milo');
+}
+
+function prependCat(name) {
+    return ['Arnold', ...cats];
+}
+
+function removeLastCat() {
+    return cats.slice(0, cats.length - 1);
+}
+
+function removeFirstCat() {
+    return cats.slice(1);
+}
